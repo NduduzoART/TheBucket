d307f43ae1e08b4bdbf4b4c3be745286a83d3980
Lahlimbazo <Lahlimbazo@hotmail.com>
2025-12-09 22:37:56 +0200

    my_hello.c

---
+++ my_hello.c
@@ -0,0 +1,22 @@
+/*-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-*\
+*                                                                                               *
+*       File Name               :       my_hello.c                                              *
+*                                                                                               *
+*       Creation Date           :       09 Dec 2025                                             *
+*                                                                                               *   *                                                                                               *
+*       created By              :       Lahlimbazo                                              *
+*                                                                                               *   \*-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-*/
+
+#include "my_library.h"
+
+//The bellow int main() is the basic prototype or structurw of a .c program
+//printf() - Displays information to the user in a structured and customizable manner.
+//printf() - Uses the header <stdio.h>
+
+
+int	main()
+{
+	printf("Hello You\n");
+
+	return (0);
+}
